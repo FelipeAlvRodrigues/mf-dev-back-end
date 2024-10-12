6ddd492e46771920944b3dc6a39599b9f26b3700
llipfe <165849851+FelipeAlvRodrigues@users.noreply.github.com>
2024-10-12 20:04:15 -0300

    Configuração do Banco de Dados e da Tabela Veículo

---
+++ mf-dev-back-end/Controllers/VeiculosController.cs
@@ -0,0 +1,13 @@
+﻿using mf_dev_back_end.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mf_dev_back_end.Controllers
+{
+    public class VeiculosController : Controller
+    {
+        private readonly AppDbContext _context;
+        public VeiculosController(AppDbContext context ) {
+            _context = context;
+        }
+    }
+}
--- mf-dev-back-end/Program.cs
+++ mf-dev-back-end/Program.cs
@@ -1,10 +1,15 @@
+using mf_dev_back_end.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddRazorPages() .AddRazorRuntimeCompilation();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
